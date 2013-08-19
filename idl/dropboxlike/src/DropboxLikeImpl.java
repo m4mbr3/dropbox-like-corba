32debffe8aa5d815a9a562b8f610e817caa8ea17
Andrea Mambretti <m4mbr3@gmail.com>
2013-08-19 23:11:10 +0200

    Added Implementation class and server class

---
+++ idl/dropboxlike/src/DropboxLikeImpl.java
@@ -0,0 +1,23 @@
+package src;
+
+import dropboxlike.FileAtRepository;
+import dropboxlike.*;
+import dropboxlike.RepositoryPOA;
+import java.util.*;
+import org.omg.CORBA.*;
+
+public class DropboxLikeImpl extends RepositoryPOA{
+    DropboxLikeImpl() {
+
+    }
+    boolean send(FileAtRepository file, String username, String token ) {
+
+    }
+    FileList askListUser (String username, String token) {
+
+    }
+    boolean delete (String filename, String username, String token) {
+
+    }
+    FileList fileList;
+}
